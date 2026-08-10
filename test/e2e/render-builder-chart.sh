f0d8340a808888418dd4d4ea660ff53abb9764ea
thanet-s <14298568+thanet-s@users.noreply.github.com>
2026-08-11 03:59:49 +0700

    Prepare 0.1.0-rc.22

--- test/e2e/render-builder-chart.sh
+++ test/e2e/render-builder-chart.sh
@@ -65,7 +65,7 @@ for kp_required in \
   "v.name == 'registry-push-credentials' && v.mountPath == '/var/run/secrets/kuberploy/registry-push'" \
   "v.name == 'registry-cache-credentials' && v.mountPath == '/var/run/secrets/kuberploy/registry-cache'" \
   "!has(v.readOnly) || v.readOnly == false" \
-  "c.image == 'registry.example.test/kuberploy/builder-agent:0.1.0-rc.21'" \
+  "c.image == 'registry.example.test/kuberploy/builder-agent:0.1.0-rc.22'" \
   "c.name == 'checkout'" \
   "c.name == 'dind'" \
   "c.command == ['/usr/local/bin/docker-init', '--', '/usr/local/bin/dockerd']" \
