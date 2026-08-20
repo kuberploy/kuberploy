6aecc686bf2bfa392ce89c3a3da404ba01f4c037
thanet-s <14298568+thanet-s@users.noreply.github.com>
2026-08-21 05:22:32 +0700

    chore(release): prepare v0.1.0-rc.277

--- cmd/kuberploy-api/platform_git_binding_test.go
+++ cmd/kuberploy-api/platform_git_binding_test.go
@@ -23,7 +23,7 @@ func platformBindingRuntime(t *testing.T) gitprojection.RuntimeConfig {
 		gitprojection.ProjectionGitAuthModeEnv:   "github-app",
 		gitprojection.ProjectionGitHubAppIDEnv:   "12345",
 		gitprojection.ProjectionGitHubClientEnv:  "Iv1_KuberployClient",
-		gitprojection.ProjectionChartVersionEnv:  "0.1.0-rc.276",
+		gitprojection.ProjectionChartVersionEnv:  "0.1.0-rc.277",
 		gitprojection.ProjectionPolicyVersionEnv: "runtime-policy-v1",
 	}
 	runtime, err := gitprojection.RuntimeConfigFromLookup(func(name string) (string, bool) {
