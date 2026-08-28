b23abfd4d4da93c4a2e61fd1bb6c632accdfd53a
thanet-s <14298568+thanet-s@users.noreply.github.com>
2026-08-28 13:21:48 +0700

    chore(release): prepare rc.397

--- test/e2e/test-kubernetes-qualification.sh
+++ test/e2e/test-kubernetes-qualification.sh
@@ -259,7 +259,7 @@ if [[ " $* " == *' template '* ]]; then
     printf '%s\n' '---' 'apiVersion: argoproj.io/v1alpha1' 'kind: Application' \
       "metadata:" "  name: kuberploy-${kp_name}" \
       '  annotations:' \
-      '    kuberploy.io/expected-package-version: "0.1.0-rc.396"' \
+      '    kuberploy.io/expected-package-version: "0.1.0-rc.397"' \
       'spec:' '  source:' \
       '    targetRevision: "0123456789abcdef0123456789abcdef01234567"'
   done
